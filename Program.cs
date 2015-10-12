f763ef1adbfafc1ab8abade7955de757cfcec14f
Ykel <yusufkel@Gmail.com>
2015-10-12 05:09:54 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            int []sorted=SortArray.Sort(new int[] { 99,67,88,1,3,2,22,18,34,25,16,8,3,4,66,44,52,2,1,2,0});
+            for (int i = 0; i < sorted.Length; i++)
+            {
+                Console.WriteLine(sorted[i]);
+            }
+            Console.Read();
+
+        }
+    }
+}
---
+++ SortArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    /// <summary>
+    /// Program to sort any array, designed by Yusuf Kelo
+    /// yusufkel@gmail.com
+    /// +0554112774
+    /// </summary>
+    public class SortArray
+    {
+        public static int[] Sort(int[] unsortedArray)
+        {
+            int[] sorted = new int[unsortedArray.Length];
+            int min = 0;
+            int temp = 0;
+            for (int i = 0; i < unsortedArray.Length; i++)
+            {
+                //check if current index is 0
+                if (i == 0)
+                {
+                    min = unsortedArray[i];
+                    sorted[i] = min;
+                }
+                //if not 0
+                else
+                {
+                    //loop untill i (current index) +1
+                    for (int s = 0; s < i+1; s++)
+                    {
+                        //check if element at i in unsortedArray is smaller than element at s in sorted array
+                        if (unsortedArray[i] < sorted[s])
+                        {
+                             min = unsortedArray[i];
+                            temp = sorted[s];
+                            //set current  sorted array index to min
+                            sorted[s] = min;
+                            //replace the element at i in unsortedArray 
+                            unsortedArray[i] = temp;
+
+                        }
+                        //else add element to the sorted array
+                        else
+                        {
+                            sorted[i] = unsortedArray[i];
+                        }
+
+                    }
+         
+                }
+
+            }
+            
+
+            return sorted;
+
+        }
+
+
+
+    }
+}
